6d002adf35b7068e0ff6ee77b16c720e2d0d9eb8
nktrnv <nktrnv@gmail.com>
2023-08-20 21:04:04 +0300

    Определить сущности Characteristic, Product и Folder

---
+++ src/entities.py
@@ -0,0 +1,30 @@
+from dataclasses import dataclass, field
+from typing import Sequence
+
+
+@dataclass(slots=True)
+class Characteristic:
+    name: str
+    value: str
+
+
+@dataclass(slots=True)
+class Product:
+    external_id: str
+    title: str
+    description: str = ""
+    text: str = ""
+    sku: str = ""
+    brand: str = ""
+    price: float | None = None
+    old_price: float | None = None
+    quantity: int | None = None
+    image_url: str = ""
+    categories: Sequence[str] = field(default_factory=list)
+    characteristics: Sequence[Characteristic] = field(default_factory=list)
+
+
+@dataclass(slots=True)
+class Folder:
+    name: str
+    nesting_level: int
